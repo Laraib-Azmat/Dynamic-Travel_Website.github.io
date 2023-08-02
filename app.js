cdec233e5734adb58c50fcc3d0bf300c5cee4162
Laraib <azmatlaraib2003@gmail.com>
2023-08-02 22:21:53 +0500

    modified code

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const mouse = document.querySelector(".cursor");
-const cursorText = document.querySelector(".cursor-text");
-const burger = document.querySelector(".burger");
-
+let controller 
+let scene;
+let scene2;
+let fash_scene;
+let fash_controller;
 
 function scrollAnimation() {
     
     // Controller
 
-    let controller = new ScrollMagic.Controller();
+     controller = new ScrollMagic.Controller();
   
     const slides = document.querySelectorAll(".sec");
     const header = document.querySelector(".main-header");
@@ -26,13 +27,13 @@ function scrollAnimation() {
         t1.fromTo(header, { y: "-100%" }, { y: "0%" }, '-=0.5');
         
         //Scene
-        let scene = new ScrollMagic.Scene({
+         scene = new ScrollMagic.Scene({
             triggerElement: slide,
             triggerHook: 0.25,
             reverse: false
         })
             .setTween(t1)
-            .addIndicators({ colorStart: "white", colorTrigger: "white", name: "Slide" })
+            // .addIndicators({ colorStart: "white", colorTrigger: "white", name: "Slide" })
             .addTo(controller);
         
         //page animation
@@ -43,7 +44,7 @@ function scrollAnimation() {
         // t2.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, '-=0.5');
 
         //page scene
-        const scene2 = new ScrollMagic.Scene({
+         scene2 = new ScrollMagic.Scene({
             triggerElement: slide,
             triggerHook: 0,
             duration: "100%",
@@ -51,7 +52,7 @@ function scrollAnimation() {
         })
             .setPin(slide, { pushFollowers: false })
             .setTween(t2)
-            .addIndicators()
+            // .addIndicators()
             .addTo(controller);
 
     });
@@ -60,11 +61,13 @@ function scrollAnimation() {
 
 
 function cursor(e) {
+    const mouse = document.querySelector(".cursor");
     mouse.style.top = e.pageY + "px";
     mouse.style.left = e.pageX + "px";
 }
 
 function cursorActive(e) {
+    const cursorText = document.querySelector(".cursor-text");
     const item = e.target;
 
     if (item.id === "logo" || item.classList.contains("burger")) {
@@ -85,6 +88,7 @@ function cursorActive(e) {
 }
 
 function openBurger() {
+    const burger = document.querySelector(".burger");
     burger.classList.toggle("active");
     if (burger.classList.contains("active")) {
         document.body.classList.add("hide");
@@ -103,6 +107,89 @@ function openBurger() {
     }
 }
 
+//Barba animation
+barba.init({
+
+    views: [
+        {
+            namespace: "home",
+            beforeEnter() {
+                scrollAnimation();
+            },
+            beforeLeave() {
+                scene.destroy();
+                controller.destroy();
+                scene2.destroy();
+            }
+        },
+        {
+            namespace: "fashion",
+            beforeEnter() {
+                secondAnimation();
+            },
+            beforeLeave() {
+                fash_scene.destroy();
+                fash_controller.destroy();
+                
+            }
+        }
+    ],
+
+    transitions: [
+        {
+            leave({ current, next }) {
+                let done = this.async;
+                const t1 = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+                t1.fromTo(".slide", 1, { x: "-100%" }, { x: "0%", onComplete: done });
+                t1.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
+            },
+       
+            enter({ current, next }) {
+                window.scrollTo(0, 0);
+                let done = this.async;
+                const t2 = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+                t2.fromTo(".slide", 1, { x: "0%" }, { x: "100%", stagger : 0.3, onComplete: done});
+                t2.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+               
+            }
+        }
+    ]
+
+});
+
+//animation on second page
+
+function secondAnimation() {
+    const fashion = document.querySelectorAll(".section");
+     fash_controller = new ScrollMagic.Controller();
+ 
+    fashion.forEach((fash, index, fashion) => {
+       
+     let nextSlide = fashion.length-1 === index ? "end" : fashion[index + 1]; 
+        const nextImg = nextSlide.querySelector("img");
+        
+       const tl =  gsap.timeline({ defaults: { duration: 1, ease: "power2.inOut" } });
+        tl.fromTo(fash,  {opacity: 1}, {opacity: 0});
+        tl.fromTo(nextSlide,  { opacity: 0 }, { opacity: 1 }, '-=1');
+        tl.fromTo(nextImg,  { x : "50%"}, { x:"0%"});
+        
+         fash_scene = new ScrollMagic.Scene({
+            triggerElement: fash,
+            triggerHook: 0,
+            duration: "100%"
+        })
+            .setPin(fash, { pushFollowers: false })
+            .setTween(tl)
+            // .addIndicators()
+            .addTo(fash_controller);
+
+    });
+
+
+}
+
+
+
 //Event Listners
 
 window.addEventListener("mousemove", cursor)
@@ -110,5 +197,3 @@ window.addEventListener("mouseover", cursorActive);
 burger.addEventListener("click", openBurger);
 
 
-
-scrollAnimation();
