5fb85558f7d207e2075281d10a4aa439496a3eb2
Nikhil Panchal <37629345+nikhilpanchal1@users.noreply.github.com>
2023-08-03 16:14:21 -0400

    Fix Barba wrapper initialization error on refresh
    Fix Barba wrapper initialization error on refresh.
    
    - Ensure Barba wrapper initializes correctly upon page refresh.
    - Adjusted the Barba container attributes and associated JavaScript.
    - Resolved the animation halt that occurs on page reload.
    
    Resolves: #IssueNumber (Not Assigned)

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 const mouse = document.querySelector(".cursor");
 const burger = document.querySelector(".burger");
 
-let controller 
+let controller
 let scene;
 let scene2;
 let fash_scene;
 let fash_controller;
 
 function scrollAnimation() {
-    
+
     // Controller
 
-     controller = new ScrollMagic.Controller();
-  
+    controller = new ScrollMagic.Controller();
+
     const slides = document.querySelectorAll(".sec");
     const header = document.querySelector(".main-header");
-    
+
     slides.forEach((slide, index) => {
         const revealImg = slide.querySelector(".reveal-img");
         const Img = slide.querySelector(".img-sec img");
         const revealText = slide.querySelector(".reveal-text");
         // const nextSlide = index == 2 ? "end" : slide[index + 1]; 
-    
+
         // GSAP
         const t1 = gsap.timeline({ default: { duration: 1, ease: "Power.inOut" } });
         t1.fromTo(revealImg, { x: "0%" }, { x: "100%" });
         t1.fromTo(Img, { scale: 2 }, { scale: 1 }, '-=0.5');
         t1.fromTo(revealText, { x: "0%" }, { x: "100%" });
         t1.fromTo(header, { y: "-100%" }, { y: "0%" }, '-=0.5');
-        
+
         //Scene
-         scene = new ScrollMagic.Scene({
+        scene = new ScrollMagic.Scene({
             triggerElement: slide,
             triggerHook: 0.25,
             reverse: false
@@ -38,7 +38,7 @@ function scrollAnimation() {
             .setTween(t1)
             // .addIndicators({ colorStart: "white", colorTrigger: "white", name: "Slide" })
             .addTo(controller);
-        
+
         //page animation
 
         const t2 = gsap.timeline();
@@ -47,7 +47,7 @@ function scrollAnimation() {
         // t2.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, '-=0.5');
 
         //page scene
-         scene2 = new ScrollMagic.Scene({
+        scene2 = new ScrollMagic.Scene({
             triggerElement: slide,
             triggerHook: 0,
             duration: "100%",
@@ -74,19 +74,19 @@ function cursorActive(e) {
 
     if (item.id === "logo" || item.classList.contains("burger")) {
         mouse.classList.add("nav_active");
-        
+
     }
-   else if (item.classList.contains("explore-button")) {
+    else if (item.classList.contains("explore-button")) {
         mouse.classList.add("btn_active");
         cursorText.innerText = "Tap";
-        gsap.to(".swipe", 1,{ y: "0%" });
+        gsap.to(".swipe", 1, { y: "0%" });
     } else {
         mouse.classList.remove("nav_active");
         mouse.classList.remove("btn_active");
         cursorText.innerText = "";
-        gsap.to(".swipe",1, { y: "100%" });
+        gsap.to(".swipe", 1, { y: "100%" });
     }
-    
+
 }
 
 function openBurger() {
@@ -109,72 +109,74 @@ function openBurger() {
 }
 
 //Barba animation
-barba.init({
+document.addEventListener("DOMContentLoaded", function () {
+    barba.init({
 
-    views: [
-        {
-            namespace: "home",
-            beforeEnter() {
-                scrollAnimation();
+        views: [
+            {
+                namespace: "home",
+                beforeEnter() {
+                    scrollAnimation();
+                },
+                beforeLeave() {
+                    scene.destroy();
+                    controller.destroy();
+                    scene2.destroy();
+                }
             },
-            beforeLeave() {
-                scene.destroy();
-                controller.destroy();
-                scene2.destroy();
-            }
-        },
-        {
-            namespace: "fashion",
-            beforeEnter() {
-                secondAnimation();
-            },
-            beforeLeave() {
-                fash_scene.destroy();
-                fash_controller.destroy();
-                
-            }
-        }
-    ],
+            {
+                namespace: "fashion",
+                beforeEnter() {
+                    secondAnimation();
+                },
+                beforeLeave() {
+                    fash_scene.destroy();
+                    fash_controller.destroy();
 
-    transitions: [
-        {
-            leave({ current, next }) {
-                let done = this.async;
-                const t1 = gsap.timeline({ defaults: { ease: "power2.inOut" } });
-                t1.fromTo(".slide", 1, { x: "-100%" }, { x: "0%", onComplete: done });
-                t1.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
-            },
-       
-            enter({ current, next }) {
-                window.scrollTo(0, 0);
-                let done = this.async;
-                const t2 = gsap.timeline({ defaults: { ease: "power2.inOut" } });
-                t2.fromTo(".slide", 1, { x: "0%" }, { x: "100%", stagger : 0.3, onComplete: done});
-                t2.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
-               
+                }
             }
-        }
-    ]
+        ],
 
+        transitions: [
+            {
+                leave({ current, next }) {
+                    let done = this.async;
+                    const t1 = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+                    t1.fromTo(".slide", 1, { x: "-100%" }, { x: "0%", onComplete: done });
+                    t1.fromTo(current.container, 1, { opacity: 1 }, { opacity: 0 });
+                },
+
+                enter({ current, next }) {
+                    window.scrollTo(0, 0);
+                    let done = this.async;
+                    const t2 = gsap.timeline({ defaults: { ease: "power2.inOut" } });
+                    t2.fromTo(".slide", 1, { x: "0%" }, { x: "100%", stagger: 0.3, onComplete: done });
+                    t2.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+
+                }
+            }
+        ]
+
+    })
 });
 
 //animation on second page
 
 function secondAnimation() {
     const fashion = document.querySelectorAll(".section");
-     fash_controller = new ScrollMagic.Controller();
- 
+    fash_controller = new ScrollMagic.Controller();
+
     fashion.forEach((fash, index, fashion) => {
-       
-     let nextSlide = fashion.length-1 === index ? "end" : fashion[index + 1]; 
+
+        let nextSlide = fashion.length - 1 === index ? "end" : fashion[index + 1];
         const nextImg = nextSlide.querySelector("img");
-        
-       const tl =  gsap.timeline({ defaults: { duration: 1, ease: "power2.inOut" } });
-        tl.fromTo(fash,  {opacity: 1}, {opacity: 0});
-        tl.fromTo(nextSlide,  { opacity: 0 }, { opacity: 1 }, '-=1');
-        tl.fromTo(nextImg,  { x : "50%"}, { x:"0%"});
-        
-         fash_scene = new ScrollMagic.Scene({
+
+        const tl = gsap.timeline({ defaults: { duration: 1, ease: "power2.inOut" } });
+        tl.fromTo(fash, { opacity: 1 }, { opacity: 0 });
+        tl.fromTo(nextSlide, { opacity: 0 }, { opacity: 1 }, '-=1');
+        tl.fromTo(nextImg, { x: "50%" }, { x: "0%" });
+
+        fash_scene = new ScrollMagic.Scene({
             triggerElement: fash,
             triggerHook: 0,
             duration: "100%"
